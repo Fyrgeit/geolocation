5db62ad6035b3533e593f796e0c0fc7137e73d5f
Fyrgeit <mans@peronilla.se>
2024-02-18 19:29:04 +0100

    with watchPosition (not working)

--- app.js
+++ app.js
@@ -11,13 +11,21 @@ let track = false;
 let upload = false;
 let lastTrack;
 let trackInterval = 15;
+let watchId;
 
 trackEl.onchange = (e) => {
     track = e.target.checked;
 
     if (track) {
-        getLocation();
+        watchId = navigator.geolocation.watchPosition(
+            success,
+            () => console.error("Unable to retrieve your location"),
+            { timeout: 10000, enableHighAccuracy: true }
+        );
+
         updateTimer();
+    } else {
+        navigator.geolocation.clearWatch(watchId);
     }
 }
 
@@ -38,50 +46,26 @@ function updateTimer() {
     setTimeout(updateTimer, 1000);
 }
 
-function getLocation() {
-    if (!track) {
-        return;
+async function success(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const timestamp = position.timestamp.toString().slice(0, -3);
+    const str = `${latitude},${longitude},${timestamp}`;
+
+    console.log(position.coords, Date.now() - lastTrack);
+    locationEl.innerText = `N${latitude} E${longitude}`;
+
+    if (upload) {
+        const månsRef = doc(db, "users", "måns");
+
+        updateDoc(månsRef, {
+            tracks: arrayUnion(str)
+        }).then(() => {
+            console.log("Document successfully written!");
+        });
     }
 
-    if (!navigator.geolocation) {
-        console.error("Geolocation is not supported by your browser");
-    } else {
-        setTimeout(getLocation, trackInterval * 1000);
-
-        navigator.geolocation.getCurrentPosition(
-            //Success
-            async (position) => {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
-                const timestamp = position.timestamp.toString().slice(0, -3);
-                const str = `${latitude},${longitude},${timestamp}`;
-
-                console.log(position.coords, Date.now() - lastTrack);
-                locationEl.innerText = `N${latitude} E${longitude}`;
-
-                if (upload) {
-                    const månsRef = doc(db, "users", "måns");
-
-                    updateDoc(månsRef, {
-                        tracks: arrayUnion(str)
-                    }).then(() => {
-                        console.log("Document successfully written!");
-                    });
-                }
-
-                lastTrack = Date.now();
-            },
-            //Fail
-            () => {
-                console.error("Unable to retrieve your location");
-            },
-            //Options
-            {
-                timeout: 10000,
-                enableHighAccuracy: true
-            }
-        );
-    }
+    lastTrack = Date.now();
 }
 
 async function exportData() {
@@ -95,15 +79,32 @@ async function exportData() {
 
     let geoJson = {
         type: "FeatureCollection",
+        features: [
+            {
+                type: "Feature",
+                properties: {},
+                geometry: {
+                    coordinates: coords.map(c => ([c.lon, c.lat])),
+                    type: "LineString"
+                }
+            }
+        ]
+    };
+
+    /* 
+    let geoJson = {
+        type: "FeatureCollection",
         features: coords.map(c => ({
-            type: "Feature",
-            properties: {},
-            geometry: {
-                coordinates: [c.lon, c.lat],
-                type: "Point"
+            {
+                type: "Feature",
+                properties: {},
+                geometry: {
+                    coordinates: [c.lon, c.lat],
+                    type: "Point"
+                }
             }
         }))
-    };
+    }; */
 
     var a = document.createElement("a");
     var file = new Blob([JSON.stringify(geoJson)], { type: "text/json" });
